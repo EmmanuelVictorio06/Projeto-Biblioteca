9b5aa7484442d2467dbcbf76d1d9faab5680e6b9
Emmanuel Victorio <emmanoelvictorio@gmail.com>
2024-10-30 23:07:35 -0300

    Testes na Main

--- Main.java
+++ Main.java
@@ -1,11 +1,30 @@
-public class Funcionario extends Usuario {
+public class Main {
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
 
-    public Funcionario(String nome, String id) {
-        super(nome, id);
-    }
+        // Adicionando livros à biblioteca
+        biblioteca.adicionarLivro(new Livro("1984", "George Orwell"));
+        biblioteca.adicionarLivro(new Livro("O Senhor dos Anéis", "J.R.R. Tolkien"));
 
-    @Override
-    public void mostrarInformacoes() {
-        System.out.println("Funcionário: " + getNome() + ", ID: " + getId());
+        // Criando usuários
+        Cliente cliente1 = new Cliente("Alice", "C001");
+        Funcionario funcionario1 = new Funcionario("Carlos", "F001");
+
+        // Adicionando usuários à biblioteca
+        biblioteca.adicionarUsuario(cliente1);
+        biblioteca.adicionarUsuario(funcionario1);
+
+        // Mostrando livros disponíveis
+        biblioteca.mostrarLivrosDisponiveis();
+
+        // Emprestando um livro
+        biblioteca.emprestarLivro("1984", cliente1);
+        biblioteca.mostrarLivrosDisponiveis();
+
+        // Devolvendo um livro
+        biblioteca.devolverLivro("1984", cliente1);
+        biblioteca.mostrarLivrosDisponiveis();
+
+        funcionario1.mostrarInformacoes();
     }
 }
